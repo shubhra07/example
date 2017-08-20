ef41c8e32046b90925da1b3db34293433f18827a
violet-ninja07 <violet.ninja07@gmail.com>
2017-08-20 06:54:10 -0700

    Initial commit progfun1 example

---
+++ src/main/scala/example/Lists.scala
@@ -0,0 +1,65 @@
+/**
+  * Updated by Shubhra Sharma on 8/18/2017
+  */
+
+package example
+
+
+object Lists {
+
+  /**
+   * This method computes the sum of all elements in the list xs. There are
+   * multiple techniques that can be used for implementing this method, and
+   * you will learn during the class.
+   *
+   * For this example assignment you can use the following methods in class
+   * `List`:
+   *
+   *  - `xs.isEmpty: Boolean` returns `true` if the list `xs` is empty
+   *  - `xs.head: Int` returns the head element of the list `xs`. If the list
+   *    is empty an exception is thrown
+   *  - `xs.tail: List[Int]` returns the tail of the list `xs`, i.e. the the
+   *    list `xs` without its `head` element
+   *
+   *  ''Hint:'' instead of writing a `for` or `while` loop, think of a recursive
+   *  solution.
+   *
+   * @param xs A list of natural numbers
+   * @return The sum of all elements in `xs`
+   */
+    def sum(xs: List[Int]): Int = {
+      def loop(xs: List[Int], acc: Int):Int = {
+        if(xs.isEmpty) acc
+        else loop(xs.tail,acc+xs.head)
+      }
+      loop(xs,0)
+    }
+  
+  /**
+   * This method returns the largest element in a list of integers. If the
+   * list `xs` is empty it throws a `java.util.NoSuchElementException`.
+   *
+   * You can use the same methods of the class `List` as mentioned above.
+   *
+   * ''Hint:'' Again, think of a recursive solution instead of using looping
+   * constructs. You might need to define an auxiliary method.
+   *
+   * @param xs A list of natural numbers
+   * @return The largest element in `xs`
+   * @throws java.util.NoSuchElementException if `xs` is an empty list
+   */
+    def max(xs: List[Int]): Int = {
+      def loop(xs : List[Int], acc:Int):Int = {
+        if (xs.isEmpty) acc
+        else {
+          val maxElement = {
+            if (acc >= xs.head) acc
+            else xs.head
+          }
+          loop(xs.tail, maxElement)
+        }
+      }
+      if(xs.isEmpty) throw new java.util.NoSuchElementException
+      else loop(xs.tail, xs.head)
+    }
+  }
